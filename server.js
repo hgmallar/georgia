68024d3381bfb4a03d1294e8965502a83da48820
Hilary Mallar <hilarymallar@gmail.com>
2020-11-11 14:17:54 -0500

    fix server

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const morgan = require('morgan')
-const nodemailer = require('nodemailer')
-const port = process.env.PORT || 4444
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const morgan = require("morgan");
+const nodemailer = require("nodemailer");
+const port = process.env.PORT || 4444;
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/sendtome', require('./routes/sendToMe'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use("/sendtome", require("./routes/sendToMe"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(port, () => {
-    console.log(`app is live on ${port}`)
-  })
+  console.log(`app is live on ${port}`);
+});
